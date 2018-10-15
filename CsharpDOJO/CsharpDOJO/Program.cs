52911cbfcda696442f003de42679847c6a64801a
Kalia Chirag <kalia.chirag@landisgyr.net>
2018-10-15 17:58:05 +0530

    Added Visual Studio Solution

---
+++ CsharpDOJO/CsharpDOJO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CsharpDOJO.MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteUsingANewThread);
+            //Worker Thread
+            thread.Start();
+            //Main Thread
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(String.Format("Main {0} ", i));
+            }
+
+        }
+
+        private static void WriteUsingANewThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(String.Format("Work {0} ", i)); // Worker Thread
+            }
+        }
+    }
+}
