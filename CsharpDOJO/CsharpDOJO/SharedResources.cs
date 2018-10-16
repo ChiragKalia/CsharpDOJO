ff104f4d39b49f9c21a13bbf3a5cf46ab13bd608
Chirag Kalia <chiragkalia1@gmail.com>
2018-10-16 13:48:56 +0530

    Added Shared Resources Example

---
+++ CsharpDOJO/CsharpDOJO/SharedResources.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System;
+using System.Threading;
+
+namespace CsharpDOJO.MultiThreading
+{
+    class SharedResources
+    {
+        private static bool isCompleted;
+        private static readonly object lockCompleted = new object();
+        static int Main(string[] args)
+        {
+            Thread thread = new Thread(HelloWorld);
+            //Worker Thread
+            thread.Start();
+            //Main Thread
+            HelloWorld();
+            return 0;
+        }
+
+        private static void HelloWorld()
+        {
+            lock (lockCompleted)
+            {
+                if(!isCompleted)
+                {
+                    isCompleted = true;
+                    Console.WriteLine("Hello World should run only once");
+                }
+            }
+        }
+    }
+}
